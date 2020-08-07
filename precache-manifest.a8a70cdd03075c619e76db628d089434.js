7d3da88ca399edfdf44b56eef884ff7a34f7b4d6
Kishan Panchal <kishanjpanchal18@gmail.com>
2020-08-07 22:51:05 +0530

    Updates

---
+++ precache-manifest.a8a70cdd03075c619e76db628d089434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3d7eb5bbf8aa74b1ede697a92da548",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "6c84ced7a54fa4f8b98c",
+    "url": "/tictactoe/static/css/main.c4a15b41.chunk.css"
+  },
+  {
+    "revision": "fefe2f25abc286699672",
+    "url": "/tictactoe/static/js/2.0db34097.chunk.js"
+  },
+  {
+    "revision": "6c84ced7a54fa4f8b98c",
+    "url": "/tictactoe/static/js/main.7bb92fa2.chunk.js"
+  },
+  {
+    "revision": "0c0aa642cecd7eeca855",
+    "url": "/tictactoe/static/js/runtime~main.1c7b51d3.js"
+  }
+]);
